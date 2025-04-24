31234ceb5f4ae6ecb96a0487f5456f9889013bd3
Gihani_Pieris <162690415+Gihanibhagya@users.noreply.github.com>
2025-04-24 12:19:08 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
-require('./otel-setup'); // Tracing setup
-// Entry point
+const client = require('prom-client');
+
+
+const collectDefaultMetrics = client.collectDefaultMetrics;
+collectDefaultMetrics();
+
+
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', client.register.contentType);
+  res.end(await client.register.metrics());
+});
